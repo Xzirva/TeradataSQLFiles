8b780612ef5082d6c7c2766fe16b1185359190a9
louisste <stephane_louis@hotmail.fr>
2017-12-01 16:28:34 +0100

    Add files via upload

--- SQL/stagingToTarget.sql
+++ SQL/stagingToTarget.sql
@@ -23,3 +23,33 @@ MERGE
     -- WHEN NOT MATCHED BY SOURCE
     --    THEN DELETE???
 ;
+
+SELECT id,
+	viewCount,
+	commentCount,
+	likeCount,
+	dislikeCount,
+	favoriteCount,
+	viewCount,
+	commentCount,
+	subscriberCount,
+	videoCount 
+	FROM prdwa17_staging.videos 
+	INNER JOIN prdwa17_staging.channels 
+	WHERE prdwa17_staging.videos.channelId=prdwa17_staging.channels.id AND prdwa17_staging.videos.fetchedAt=prdwa17_staging.channels.fetchedAt
+	AS Results;
+
+INSERT INTO prdwa17_target.t_videofacts (code_video,t_viewcount, t_commentcount, t_likecount,t_dislikecount,t_favoritecount,t_actualdate, t_channelcommentscount, t_channelsubscribercount, t_channelvideocount)
+SELECT v.id,
+	v.viewCount,
+	v.commentCount,
+	v.likeCount,
+	v.dislikeCount,
+	v.favoriteCount,
+	v.fetchedAt,
+	c.commentCount,
+	c.subscriberCount,
+	c.videoCount 
+	FROM prdwa17_staging.videos as v
+	INNER JOIN prdwa17_staging.channels as c
+	ON v.channelid=c.id AND v.fetchedAt=c.fetchedAt;
